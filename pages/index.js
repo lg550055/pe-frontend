fef6daf49d1806a5818bb57a6beee8a6e9d43eec
lg550055 <lg550055@hotmail.com>
2022-06-15 17:58:21 -0500

    Adds p/e calculations

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,23 @@ import { useState, useEffect } from 'react'
 export default function Home() {
 
   const [estimates, setEstimates] = useState([])
+  const [prices, setPrices] = useState({})
 
   const url = process.env.NEXT_PUBLIC_RESOURCE_URL
+  const token = process.env.NEXT_PUBLIC_TOKEN
+
+  function get_prices(estimates) {
+    let temp_p = {}
+    estimates.forEach(async e => {
+      try {
+        let p_data = await axios.get(`https://finnhub.io/api/v1/quote?token=${token}&symbol=${e.symbol}`)
+        temp_p[e.symbol] = p_data.data.c  
+      } catch (e) {
+        console.log(e)
+      }
+    })
+    setPrices(temp_p)
+  }
 
   useEffect(() => {
     axios.get(url).then(e => setEstimates(e.data))
@@ -24,7 +39,8 @@ export default function Home() {
       <main className={styles.main}>
         <h2>Simple eps estimates</h2>
         <h3>Current eps estimates for 2022 and 2023</h3>
-        {estimates.map(e => <p key={e.id}>{e.symbol} : {e.fwd_eps}, {e.fwd2_eps}</p>)}
+        { prices && estimates.map(e => <p key={e.id}>{e.symbol} {prices[e.symbol]} {e.fwd_eps} {prices[e.symbol]/e.fwd_eps} {e.fwd2_eps} {prices[e.symbol]/e.fwd2_eps}</p>)}
+        <button onClick={()=>get_prices(estimates)}>Prices</button>
       </main>
 
       <footer className={styles.footer}>
