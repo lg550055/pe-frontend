87ee5bfab0737d3fa18a23292f2c9e87ee3f955a
lg550055 <lg550055@hotmail.com>
2022-07-09 17:14:17 -0500

    Adds tev and related columns

--- components/estimates.js
+++ components/estimates.js
@@ -5,26 +5,36 @@ export default function Estimates({ estimates, prices }) {
       <thead>
         <tr>
           <th>Ticker</th>
+          <th>Industry</th>
           <th>Price</th>
+          <th>MCap</th>
+          <th>TEV</th>
           <th>eps22</th>
           <th>eps23</th>
           <th>r_g22</th>
           <th>r_g23</th>
           <th>p/e22</th>
           <th>p/e23</th>
+          <th>T/ebitda</th>
+          <th>T/e-cpx</th>
         </tr>
       </thead>
       <tbody>
         {estimates.map(e => 
           <tr key={e.id}>
             <th>{e.symbol}</th>
+            <td>{e.industry ? e.industry.slice(0,11) : null}</td>
             <td>{prices[e.symbol]}</td>
+            <td>{(prices[e.symbol]*e.shrs_out).toFixed()}</td>
+            <td>{e.ndebt1 ? (prices[e.symbol]*e.shrs_out + e.ndebt1*1).toFixed() : null}</td>
             <td>{e.fwd_eps}</td>
             <td>{e.fwd2_eps}</td>
             <td>{(e.fwd_rev_g * 100).toFixed()}%</td>
             <td>{(e.fwd2_rev_g * 100).toFixed()}%</td>
             <td>{(prices[e.symbol]/e.fwd_eps).toFixed(1)}</td>
             <td>{(prices[e.symbol]/e.fwd2_eps).toFixed(1)}</td>
+            <td>{e.ndebt1 ? ((prices[e.symbol]*e.shrs_out + e.ndebt1*1)/e.trail_ebitda).toFixed(1) : null}</td>
+            <td>{e.ndebt1 ? ((prices[e.symbol]*e.shrs_out + e.ndebt1*1)/(e.trail_ebitda-e.trail_capex)).toFixed(1) : null}</td>
           </tr>
         )}
       </tbody>
